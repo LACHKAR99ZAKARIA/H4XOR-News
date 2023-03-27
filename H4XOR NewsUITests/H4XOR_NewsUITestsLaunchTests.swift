a7e7b4e8b49b009b3efea2aa226ee4389f74c8b2
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-03-27 10:52:26 +0000

    Initial Commit

---
+++ H4XOR NewsUITests/H4XOR_NewsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  H4XOR_NewsUITestsLaunchTests.swift
+//  H4XOR NewsUITests
+//
+//  Created by zakaria lachkar on 27/03/2023.
+//
+
+import XCTest
+
+final class H4XOR_NewsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
